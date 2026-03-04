3131b754df74068ba45b1cc403b91df922e9d9c1
Lakshmipriya89777 <lakshmipriyavikruthamala@gmail.com>
2026-03-04 09:53:20 +0530

    Update search.js

--- script/search.js
+++ script/search.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Get input element
 const input = document.getElementById("searchBox");
 
@@ -83,89 +82,5 @@ function searchTemple() {
     alert("This temple is not available on this website.");
   }
 }
-=======
-// Get input element
-const input = document.getElementById("searchBox");
 
-// Search keywords mapped to pages
-const templeMap = {
-  varaha: "varaha.html",
-  "varaha swami temple": "varaha.html",
 
-  tirumala: "tirumala.html",
-  "tirumala temple": "tirumala.html",
-
-  papavinasanam: "papavinasanam.html",
-  akasaganga: "akasaganga.html",
-  japali: "japali.html",
-  silathoranam: "silathoranam.html",
-
-  venugopala: "venugopala.html",
-  "venugopala swami temple": "venugopala.html",
-
-  "srivari padhalu": "padhalu.html",
-  padhalu: "padhalu.html",
-
-  kapilatheertham: "kapilatheertham.html",
-  museum: "museum.html",
-  "tirumala museum": "museum.html",
-
-  padmavathi: "padmavathi.html",
-  "padmavathi temple": "padmavathi.html",
-  tiruchanur: "padmavathi.html",
-
-  "lalitha peetam": "lalitha.html",
-
-  iskcon: "iskcon.html",
-  "iskcon temple": "iskcon.html",
-
-  kodhanda: "kodhanda.html",
-  kodhandarama: "kodhanda.html",
-
-  mukkoti: "mukkoti.html",
-
-  govindharaja: "govindharaja.html",
-  "govindharaja temple": "govindharaja.html",
-
-  "srinivasa mangapuram": "kalyana.html",
-  "kalyana venkateswara swami temple": "kalyana.html",
-
-  vakulamatha: "vakulamatha.html",
-  jain: "jain.html",
-
-  varahi: "varahi.html",
-  "varahi ammavaari temple": "varahi.html",
-
-  gudimallam: "gudimallam.html",
-
-  appalayagunta: "appalayagunta.html",
-  "prasanna venkateswara swami temple": "appalayagunta.html",
-
-  kalahasthi: "kalahasthi.html",
-  "kalahasthi temple": "kalahasthi.html",
-
-  thondamanadu: "thondamanadu.html",
-
-  kanipakam: "kanipakam.html",
-  "kanipakam temple": "kanipakam.html",
-};
-
-// Enter key support
-input.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    searchTemple();
-  }
-});
-
-// Search function
-function searchTemple() {
-  const query = input.value.trim().toLowerCase();
-
-  if (templeMap[query]) {
-    window.location.href = "../htmlfiles/" + templeMap[query];
-  } else {
-    alert("This temple is not available on this website.");
-  }
-}
->>>>>>> 64e9530ca4be6a3b6e2323644518346eb52f6325
